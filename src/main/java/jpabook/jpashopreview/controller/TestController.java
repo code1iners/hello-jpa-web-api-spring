1ff5784bc4f37eccbdf16f1fbfcf009ec8de027f
Codeliner <codeliner96@gmail.com>
2021-05-21 14:57:46 +0900

    Added index, test html with controller.

---
+++ src/main/java/jpabook/jpashopreview/controller/TestController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashopreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "test";
+    }
+
+}
