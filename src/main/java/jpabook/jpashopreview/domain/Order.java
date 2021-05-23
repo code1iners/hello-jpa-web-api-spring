ed0bbbc471e0e2fb02492fb2124a64e2627124e1
Codeliner <codeliner96@gmail.com>
2021-05-23 15:43:58 +0900

    Added business logics & other logics in Order, OrderItem

--- src/main/java/jpabook/jpashopreview/domain/Order.java
+++ src/main/java/jpabook/jpashopreview/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashopreview.domain;
 
+import jpabook.jpashopreview.domain.status.DeliveryStatus;
 import jpabook.jpashopreview.domain.status.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,4 +34,74 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    // note. Bidirectional convenience methods.
+
+    /**
+     * <h3>Set order item</h3>
+     * <p>Convenience method.</p>
+     */
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    /**
+     * <h3>Set delivery</h3>
+     * <p>Convenience method.</p>
+     */
+    public void addDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    // note. Order method.
+
+    /**
+     * <h3>Create new order</h3>
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem: orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // note. Business logics.
+    /**
+     * <h3>Cancel order</h3>
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("Already arrived. Order cancel is imposible.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * <h3>Look up</h3>
+     * <p>Get total price of all items.</p>
+     */
+    public int getTotalPrice() {
+//        int totalPrice = 0;
+//
+//        for (OrderItem orderItem : orderItems) {
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 }
