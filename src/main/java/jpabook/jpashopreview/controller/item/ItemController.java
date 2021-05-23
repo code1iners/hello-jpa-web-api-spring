be84933917c5ec0e72cb7da8924593e363734ca8
Codeliner <codeliner96@gmail.com>
2021-05-23 18:00:54 +0900

    Added items controller.

---
+++ src/main/java/jpabook/jpashopreview/controller/item/ItemController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashopreview.controller.item;
+
+import jpabook.jpashopreview.domain.item.Book;
+import jpabook.jpashopreview.domain.item.Item;
+import jpabook.jpashopreview.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        // note. Set book entity.
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        // note. Save item.
+        itemService.saveItem(book);
+
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
