3fbc0e22b5617c79f46319134c3c0a9387725565
Codeliner <codeliner96@gmail.com>
2021-05-24 14:39:03 +0900

    Added get orders v2 with Order DTO

--- src/main/java/jpabook/jpashopreview/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashopreview/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashopreview.api;
 
 import jpabook.jpashopreview.domain.Order;
+import jpabook.jpashopreview.domain.status.OrderStatus;
+import jpabook.jpashopreview.domain.value.Address;
 import jpabook.jpashopreview.repository.OrderRepository;
 import jpabook.jpashopreview.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <H3>Relations xToOne(ManyToOne, OneToOne)</H3>
@@ -33,5 +38,35 @@ public class OrderSimpleApiController {
             foundOrder.getDelivery().getAddress(); // note. Force init lazy.
         }
         return foundOrders;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2() {
+        List<Order> foundOrders = orderRepository.search(new OrderSearch());
+        List<SimpleOrderDTO> result = foundOrders.stream()
+                .map(SimpleOrderDTO::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    // note. DTO...
+    /**
+     * <h3>Simple order DTO</h3>
+     */
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
