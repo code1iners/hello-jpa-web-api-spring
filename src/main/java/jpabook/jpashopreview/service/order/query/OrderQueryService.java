21a3f174e817b10a7043de5089bef01fe7eeb83e
Codeliner <codeliner96@gmail.com>
2021-05-28 09:19:23 +0900

    Added OSIV setting.

--- src/main/java/jpabook/jpashopreview/service/order/query/OrderQueryService.java
+++ src/main/java/jpabook/jpashopreview/service/order/query/OrderQueryService.java
@@ -1,16 +1,28 @@
 package jpabook.jpashopreview.service.order.query;
 
-import jpabook.jpashopreview.api.OrderApiController;
+import jpabook.jpashopreview.domain.Order;
+import jpabook.jpashopreview.domain.order.OrderDto;
+import jpabook.jpashopreview.repository.order.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class OrderQueryService {
 
-//    public List<OrderDto> orders() {
-//
-//    }
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> orders() {
+        List<Order> foundOrders = orderRepository.findAllWithItem();
+        List<OrderDto> result = foundOrders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
 }
